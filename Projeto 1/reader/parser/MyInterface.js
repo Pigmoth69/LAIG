17e85c006b46b8dc323056aba7e562940c7af6c4
JCB <pigmoth@gmail.com>
2015-10-18 19:33:41 +0100

    lsx

--- Projeto 1/reader/parser/MyInterface.js
+++ Projeto 1/reader/parser/MyInterface.js
@@ -49,11 +49,11 @@ MyInterface.prototype.init = function(application) {
 MyInterface.prototype.loadInterfaceLigths = function(){
     var group = this.gui.addFolder('Lights');
     group.open();
-    var save = this;
+    var save = this.scene;
 	for(key in this.scene.graph.lightsStateValue){
 	    var action = group.add(this.scene.graph.lightsStateValue,key);
 	    action.onChange(function(value) {
-	    	save.scene.changeLight(this.property, value);
+	    	save.changeLight(this.property, value);
 	    });
 	}
 };
