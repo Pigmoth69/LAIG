7a87709011c1e0b6c5b1c70e9cf10deab5c3cba0
guilhermevpinto <guilhermevpinto@gmail.com>
2015-12-21 13:59:13 +0000

    clean copy

--- Projeto Final - Meerkats - Cópia/project/reader/parser/Illumination.js
+++
@@ -1,7 +0,0 @@
-function Illumination() {
-    this.ambient = [0, 0, 0, 1];
-    this.background = [0, 0, 0, 1];
-}
-
-Illumination.prototype = Object.create(Object.prototype);
-Illumination.prototype.constructor = Illumination;
